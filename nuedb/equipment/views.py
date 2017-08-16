ec26401e5233953b387d75f7377d59ef35441884
Sanat <kvenecusghost@gmail.com>
2017-08-16 16:56:41 +0600

    listview 1

--- nuedb/equipment/views.py
+++ nuedb/equipment/views.py
@@ -3,4 +3,10 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 
+from django.http import HttpResponse
+
+def home(request):
+	return HttpResponse('Hello')
+
+
 # Create your views here.
